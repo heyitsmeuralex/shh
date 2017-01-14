26cc44279927a11409960caaee7b1ab1d7d01fb5
Alex <16batesa@gmail.com>
2017-01-14 20:15:30 +0000

    Implement basic PIXI magic

--- client/js/shh.js
+++ client/js/shh.js
@@ -1,1 +1,23 @@
-console.log('be quiet')
+const PIXI = require('pixi.js')
+
+let renderer = new PIXI.WebGLRenderer(256, 256, {
+  roundPixels: true,
+  antialias: true,
+})
+
+// Pretty background colour
+renderer.backgroundColor = 0x111929
+
+// Upscale renderer to size of browser window
+renderer.autoResize = true
+renderer.view.style.position = 'absolute'
+renderer.view.style.display = 'block'
+renderer.resize(window.innerWidth, window.innerHeight)
+
+// Add canvas to document body & apply some styles
+document.body.style.margin = 0
+document.body.style.padding = 0
+document.body.appendChild(renderer.view)
+
+let stage = new PIXI.Container()
+renderer.render(stage)
