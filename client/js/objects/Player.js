a26062453bd3e35e89d5de8946536c07d7f26149
Alex <16batesa@gmail.com>
2017-01-14 21:00:09 +0000

    Add a player

---
+++ client/js/objects/Player.js
@@ -0,0 +1,30 @@
+module.exports = class Player extends PIXI.Graphics {
+  constructor(...args) {
+    super(...args)
+
+    // Draw rounded rectangle w/ cyan stroke
+    this.lineStyle(10, 0x00ffff)
+    this.drawRoundedRect(0, 0, 60, 60, 25)
+
+    // Rotate from the centre of the sprite
+    this.pivot.x = this.width / 2
+    this.pivot.y = this.height / 2
+
+    this.mouse = { x: 0, y: 0 }
+    window.addEventListener('mousemove', evt => {
+      this.mouse.x = evt.clientX
+      this.mouse.y = evt.clientY
+    })
+  }
+
+  update() {
+    this.x++
+    this.y++
+
+    this.clear()
+
+    // Rotate towards mouse
+    let targetRotation = Math.atan2(this.mouse.y - this.y, this.mouse.x - this.x)
+    this.rotation += (targetRotation - this.rotation) / 5
+  }
+}
--- client/js/shh.js
+++ client/js/shh.js
@@ -1,4 +1,5 @@
 const PIXI = require('pixi.js')
+const Player = require('./objects/Player')
 
 let renderer = new PIXI.WebGLRenderer(256, 256, {
   roundPixels: true,
@@ -14,10 +15,30 @@ renderer.view.style.position = 'absolute'
 renderer.view.style.display = 'block'
 renderer.resize(window.innerWidth, window.innerHeight)
 
+// Automagically resize to browser window size
+window.addEventListener('resize', () =>
+  renderer.resize(window.innerWidth, window.innerHeight))
+
 // Add canvas to document body & apply some styles
 document.body.style.margin = 0
 document.body.style.padding = 0
 document.body.appendChild(renderer.view)
 
+// Where every sprite lives: the stage
 let stage = new PIXI.Container()
-renderer.render(stage)
+let objects = []
+
+// Create the player
+let player = new Player()
+stage.addChild(player)
+objects.push(player)
+
+// Render loop
+window.requestAnimationFrame(function render(timestamp) {
+  for (let object of objects) {
+    object.update()
+  }
+
+  renderer.render(stage)
+  window.requestAnimationFrame(render)
+})
