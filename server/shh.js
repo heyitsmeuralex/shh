8410eceace3ee8485b28f27f3060ec93186f90c0
an-OK-squirrel <an-OK-squirrel@users.noreply.github.com>
2017-01-14 16:18:16 -0600

    Revert "fix garbage"
    This reverts commit f068469f6a909cd47c4dbac5d8cec920adb0929e.

--- server/shh.js
+++ server/shh.js
@@ -2,8 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.disable('x-powered-by')
-app.disable('x-content-type-options')
 app.use(express.static(path.join(__dirname, '../client')))
 
 app.listen(7000, () => {
